79955e4d34fe50417178b467383e4aa0891d64c8
Zheng Wang <wzheng2013@gmail.com>
2021-01-30 13:27:39 -0500

    writting db functions

--- db.py
+++ db.py
@@ -22,19 +22,35 @@ class User(Document):
     username = StringField(required = True, unique = True)
     email = StringField(required = True, unique = True)
     password = StringField(required = True)
+    watchList = SortedListField(EmbeddedDocumentField(Stock), ordering = 'name')
     balence = FloatField(default = 10000.00)
     ownedStock = SortedListField(EmbeddedDocumentField(OwnedStock), ordering = 'name')
     totalStockValue = FloatField(default = 0.00)
-    watchList = SortedListField(EmbeddedDocumentField(Stock), ordering = 'name')
     meta = {
         'ordering': ['+username']
     }
 
+class StockInfo(Document):
+    allStocks = SortedListField(EmbeddedDocumentField(Stock), ordering = 'name')
+
 
 def newUser(username, email, password):
     if User.objects(username = username):
+        return  'username already exist'
+    if User.objects(email = email):
+        return 'email address already used'
+    user = User(username, email, password)
+    # apple = newStock(apple)
+    # use the scrape to get info by name
+    # then store the info into watchlist
+    # defualt watclist: apple, google, amazon.........
+    user.save()
+    return 'success'
 
+def newStock(name, symbol, price, delta):
+    if 
 
+#def updateStockInfo()
 
 
 

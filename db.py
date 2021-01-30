8b701ad8bc465cf47df7e9ea57802b485378bf6e
Zheng Wang <wzheng2013@gmail.com>
2021-01-30 14:41:52 -0500

    db functions

--- db.py
+++ db.py
@@ -1,18 +1,20 @@
 from mongoengine import Document, connect
 from mongoengine.document import EmbeddedDocument
 from mongoengine.fields import EmbeddedDocumentField, FloatField, SortedListField, StringField
+import json
 
-connect('StockSimulator')
+
+connect('test')
 
 class Stock(EmbeddedDocument):
-    name = StringField(required = True)
     symbol = StringField(required = True)
+    name = StringField(required = True)
     price = FloatField(required = True)
     delta = FloatField(required = True)
 
 
 class OwnedStock(EmbeddedDocument):
-    name = StringField(required = True)
+    symbol = StringField(required = True)
     stock = EmbeddedDocumentField(Stock, required = True)
     shareCount = FloatField(required = True)
     value = FloatField(required = True)
@@ -22,9 +24,9 @@ class User(Document):
     username = StringField(required = True, unique = True)
     email = StringField(required = True, unique = True)
     password = StringField(required = True)
-    watchList = SortedListField(EmbeddedDocumentField(Stock), ordering = 'name')
+    watchList = SortedListField(EmbeddedDocumentField(Stock), ordering = 'symbol')
     balence = FloatField(default = 10000.00)
-    ownedStock = SortedListField(EmbeddedDocumentField(OwnedStock), ordering = 'name')
+    ownedStock = SortedListField(EmbeddedDocumentField(OwnedStock), ordering = 'symbol')
     totalStockValue = FloatField(default = 0.00)
     meta = {
         'ordering': ['+username']
@@ -47,10 +49,37 @@ def newUser(username, email, password):
     user.save()
     return 'success'
 
-def newStock(name, symbol, price, delta):
-    if 
+def newStock(symbol, name, price, delta):
+    if len(StockInfo.objects()) > 0:
+        allStocks = StockInfo.objects().first().allStocks()
+        for stock in allStocks:
+            if symbol == stock.symbol:
+                return 'stock info already exist in db'
+        print('adding new stock')
+        stock = Stock(symbol, name, price, delta)
+        allStocks.append(stock)
+        StockInfo.objects().first().save()
+        print('new stock addede')
+    else:
+        print('first time adding stock, creating new stock list')
+        newStockList = StockInfo()
+        stock = Stock(symbol, name, price, delta)
+        newStockList.allStocks.append(stock)
+        newStockList.save()
+        print('first stock added in the liust')
+    return('success')
+        
+newStock('APL', 'Apple', 2.333, 12.22297)
+# newUser('a', 'a.casda', 'asd')
+print(json.dumps(json.loads(StockInfo.objects().to_json()), sort_keys=True, indent=4))
 
 #def updateStockInfo()
+
+# print('hi')
+# print(StockInfo.objects())
+# allStocks = StockInfo.objects().first().allStocks()
+# print(allStocks)
+# print('hi again')
 
 
 
